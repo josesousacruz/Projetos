a137caf8e58994229bdf234ef9ce5f902a625972
jOSEsOUSAFh <89113025+jOSEsOUSAFh@users.noreply.github.com>
2023-07-03 23:25:17 -0300

    att

--- Projetos-PHP/Status-de-Producao/arquivos.php
+++ Projetos-PHP/Status-de-Producao/arquivos.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include('includes/header.php');
 include('includes/navbar.php');
 ?>
@@ -9,9 +9,9 @@ include('includes/navbar.php');
     <div id="content">
         <!-- navbar fixed-top navbar-light bg-light -->
         <!-- Topbar -->
-        <?php 
-                include('includes/topbar.php');
-                ?>
+        <?php
+        include('includes/topbar.php');
+        ?>
         <!-- End of Topbar -->
 
         <!-- Begin Page Content -->
@@ -31,20 +31,20 @@ include('includes/navbar.php');
         </div>
 
         <div class="card-body">
-            <?php 
-                            
-                            if(isset($_SESSION['success']) && $_SESSION['success'] !='') {
-                                echo '<h2 class="bg-primary">' .$_SESSION['success'] . '</h2> ';
-                                unset($_SESSION['success']);
-                            }
+            <?php
 
-                            
-                            if(isset($_SESSION['status']) && $_SESSION['status'] !='') {
-                                echo '<h2 class="bg-info">' .$_SESSION['status'] . '</h2> ';
-                                unset($_SESSION['status']);
-                            }
-                            
-                            ?>
+            if (isset($_SESSION['success']) && $_SESSION['success'] != '') {
+                echo '<h2 class="bg-primary">' . $_SESSION['success'] . '</h2> ';
+                unset($_SESSION['success']);
+            }
+
+
+            if (isset($_SESSION['status']) && $_SESSION['status'] != '') {
+                echo '<h2 class="bg-info">' . $_SESSION['status'] . '</h2> ';
+                unset($_SESSION['status']);
+            }
+
+            ?>
 
             <div class="card shadow border-left-success">
                 <table class="display nowrap compact table table-sm table-bordered table-hover" id="dataTable" width="100%" cellspacing="0">
@@ -79,83 +79,84 @@ include('includes/navbar.php');
                         <tbody class="" id="results">
 
                             <?php
-            $sql = "SELECT c.*,
-            TIMEDIFF(c.data_fim, c.data_inicio) AS tempo_carregamento,
-            p.id AS produto_id,
-            p.nome AS nome_produto,
-            t.id AS transportadora_id,
-            t.razao_social AS nome_transportador,
-            DATE_FORMAT(c.data_chegada, '%d/%m/%Y %H:%i:%s') AS data_chegada,
-            DATE_FORMAT(c.data_inicio, '%d/%m/%Y %H:%i:%s') AS data_inicio,
-            DATE_FORMAT(c.data_fim, '%d/%m/%Y %H:%i:%s') AS data_fim
-            FROM carregamentos c
-            INNER JOIN produto p ON c.id_produto = p.id
-            INNER JOIN transportador t ON c.id_transportador = t.id
-            WHERE c.status_carregamento = 'Liberado' OR c.status_carregamento = 'Cancelado'
-            ORDER BY idOrdem ASC;";
-            $query = $conection->prepare($sql);
-            $query->execute();
-            $resultado = $query->fetchAll(PDO::FETCH_OBJ);
+                            $sql = "SELECT c.*,
+                            TIMEDIFF(c.data_fim, c.data_inicio) AS tempo_carregamento,
+                            p.id AS produto_id,
+                            p.nome AS nome_produto,
+                            t.id AS transportadora_id,
+                            t.razao_social AS nome_transportador,
+                            DATE_FORMAT(c.data_chegada, '%d/%m/%Y %H:%i:%s') AS data_chegada,
+                            DATE_FORMAT(c.data_inicio, '%d/%m/%Y %H:%i:%s') AS data_inicio,
+                            DATE_FORMAT(c.data_fim, '%d/%m/%Y %H:%i:%s') AS data_fim
+                            FROM carregamentos c
+                            INNER JOIN produto p ON c.id_produto = p.id
+                            INNER JOIN transportador t ON c.id_transportador = t.id
+                            WHERE c.status_carregamento = 'Liberado' OR c.status_carregamento = 'Cancelado'
+                            ORDER BY idOrdem ASC;";
+                            $query = $conection->prepare($sql);
+                            $query->execute();
+                            $resultado = $query->fetchAll(PDO::FETCH_OBJ);
 
-            foreach($resultado as $retorno){ 
+                            foreach ($resultado as $retorno) {
 
-            $tempoCarregamento;
-            if ($retorno->tempo_carregamento < "00:00:00") {
-                $tempoCarregamento = "00:00:00";
-            } elseif ($retorno->status_carregamento <> "Liberado") {
-                $tempoCarregamento = "00:00:00";
-            } else {
-                $tempoCarregamento = $retorno->tempo_carregamento;
-            }
+                                $tempoCarregamento;
+                                if ($retorno->tempo_carregamento < "00:00:00") {
+                                    $tempoCarregamento = "00:00:00";
+                                } elseif ($retorno->status_carregamento <> "Liberado") {
+                                    $tempoCarregamento = "00:00:00";
+                                } else {
+                                    $tempoCarregamento = $retorno->tempo_carregamento;
+                                }
 
-        ?>
+                            ?>
 
-                            <tr class="text-center text-dark tabelaSmall" id="<?php echo $retorno->id;?>">
-                                <td><?php echo $retorno->data_chegada ;?></td>
-                                <td><?php echo $retorno->placa ;?></td>
-                                <td><?php echo $retorno->nome_produto ;?></td>
-                                <td><?php echo $retorno->especie ;?></td>
-                                <td><?php echo $retorno->quantidade ;?></td>
-                                <td><?php echo $retorno->data_inicio ?></td>
-                                <td><?php echo $retorno->data_fim?></td>
-                                <td><?php echo $tempoCarregamento?></td>
-                                <td <?php
+                                <tr class="text-center text-dark tabelaSmall" id="<?php echo $retorno->id; ?>">
+                                    <td><?php echo $retorno->data_chegada; ?></td>
+                                    <td><?php echo $retorno->placa; ?></td>
+                                    <td><?php echo $retorno->nome_produto; ?></td>
+                                    <td><?php echo $retorno->especie; ?></td>
+                                    <td><?php echo $retorno->quantidade; ?></td>
+                                    <td><?php echo $retorno->data_inicio ?></td>
+                                    <td><?php echo $retorno->data_fim ?></td>
+                                    <td><?php echo $tempoCarregamento ?></td>
+                                    <td <?php
 
-                                        if ($retorno->status_carregamento == 'Pátio' || 
-                                        $retorno->status_carregamento == 'Aguardando OP') {
+                                        if (
+                                            $retorno->status_carregamento == 'Pátio' ||
+                                            $retorno->status_carregamento == 'Aguardando OP'
+                                        ) {
                                             echo "class='badge rounded-pill text-dark bg-warning' ";
-                                        }else if ($retorno->status_carregamento == 'Em carregamento') {
+                                        } else if ($retorno->status_carregamento == 'Em carregamento') {
                                             echo "class='badge rounded-pill  bg-primary'";
-                                        }else if($retorno->status_carregamento == 'Ajuste de peso'){
+                                        } else if ($retorno->status_carregamento == 'Ajuste de peso') {
                                             echo "class='badge rounded-pill text-dark bg-info'";
-                                        }else if($retorno->status_carregamento == 'Programado'){
+                                        } else if ($retorno->status_carregamento == 'Programado') {
                                             echo "class='badge rounded-pill  bg-secondary'";
-                                        }else if($retorno->status_carregamento == 'Cancelado'){
+                                        } else if ($retorno->status_carregamento == 'Cancelado') {
                                             echo "class='badge rounded-pill  bg-danger'";
                                         } else {
                                             echo "class='badge rounded-pill  bg-success'";
                                         }
 
-                                        ?>><?php echo $retorno->status_carregamento;?></td>
-                                <td><?php echo $retorno->nome_transportador ;?></td>
-                                <td><?php echo $retorno->ordemProd ;?></td>
-                                <td><?php echo $retorno->cif_fob;?></td>
-                                <td><?php echo $retorno->n_pedido;?></td>
-                                <td><?php echo $retorno->produtor;?></td>
-                                <td><?php echo $retorno->nf_retorno;?></td>
-                                <td><?php echo $retorno->ticket;?></td>
-                                <td><?php echo $retorno->nf_venda;?></td>
+                                        ?>><?php echo $retorno->status_carregamento; ?></td>
+                                    <td><?php echo $retorno->nome_transportador; ?></td>
+                                    <td><?php echo $retorno->ordemProd; ?></td>
+                                    <td><?php echo $retorno->cif_fob; ?></td>
+                                    <td><?php echo $retorno->n_pedido; ?></td>
+                                    <td><?php echo $retorno->produtor; ?></td>
+                                    <td><?php echo $retorno->nf_retorno; ?></td>
+                                    <td><?php echo $retorno->ticket; ?></td>
+                                    <td><?php echo $retorno->nf_venda; ?></td>
 
-                                <td class="text-center d-sm-flex align-items-center">
-                                    <form action="edit.php" method="post">
-                                        <input type="hidden" name="edit-id" value="<?php echo $retorno->id; ?>">
-                                        <button type='submit' name="edit-btn" id='edite'
-                                            class='fa fa-magnifying-glass-plus btn-sm btn-success m-1'></button>
+                                    <td class="text-center d-sm-flex align-items-center">
+                                        <form action="edit.php" method="post">
+                                            <input type="hidden" name="edit-id" value="<?php echo $retorno->id; ?>">
+                                            <button type='submit' name="edit-btn" id='edite' class='fa fa-magnifying-glass-plus btn-sm btn-success m-1'></button>
 
-                                    </form>
-                                </td>
-                                </td>
-                            </tr>
+                                        </form>
+                                    </td>
+                                    </td>
+                                </tr>
 
                             <?php } ?>
                         </tbody>
@@ -184,7 +185,7 @@ include('includes/navbar.php');
 
 
 
-<?php 
+<?php
 
 include('includes/modal.php');
 include('includes/footer.php');
